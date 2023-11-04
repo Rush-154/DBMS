d632aa7ffdf013dc1353c66f1375e4623b581d94
Aditya Khode <113977001+adityakhode@users.noreply.github.com>
2023-11-05 00:44:18 +0530

    Update after_login.py

--- Login/after_login.py
+++ Login/after_login.py
@@ -1,8 +1,14 @@
 import customtkinter as ctk
+import time
+import flexcards as cards
+import databases as db
+
 
 with open("username.txt", "r") as file:
     # Read the first line
     first_line = file.readline()
+    first_line = db.get_name_by_value(first_line)
+    db.conn.close()
 username = 'Hello ' + first_line
 
 def frontend_initiliesd():
@@ -21,13 +27,17 @@ def structure():
     label = ctk.CTkLabel(master=apps, text=username)
     label.place(relx=0.01, rely=0.0, anchor="nw")
 
+    date = time.strftime('%d / %m / %Y')
+    label = ctk.CTkLabel(master=apps, text=date)
+    label.place(relx=0.5, rely=0, anchor="n")
+    
     # Create a horizontal line
     line = ctk.CTkCanvas(master=apps, width=apps.winfo_screenwidth(), height=2)
     line.create_rectangle(0, 0, apps.winfo_screenwidth(), 2, fill="blue")
     line.place(relx=0, rely=0.04, anchor="nw")  # Adjust the rely value to position the line
     
     # create a Create Button
-    create_button = ctk.CTkButton(master=apps, text='Create +', command=create)
+    create_button = ctk.CTkButton(master=apps, text='Create_class +', command=create_class)
     create_button.place(relx=0.059, rely=0.075, anchor="center")
     
     # Create a vertical line
@@ -36,8 +46,12 @@ def structure():
     
 
 
-def create():
-    pass
+def create_class():
+    app   = cards.QApplication(cards.sys.argv)
+    popup = cards.Addsubject()
+    result= popup.exec()
+    if result == cards.QDialog.Accepted:
+        subject_name , teacher_name , num_students = popup.create_class_data()
 
 apps = frontend_initiliesd()
 structure()
